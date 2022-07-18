e3848df057ed0e8d11345dfb4e3b88b4d9bbc077
Олег Малютин <91494915+Le-Ga@users.noreply.github.com>
2022-07-18 14:52:10 +0300

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ function Footer() {
           © {new Date().getFullYear()} Copyright Text
           <a
             className="grey-text text-lighten-4 right"
-            href="https://github.com/Le-Ga/"
+            href="https://github.com/Le-Ga/react-spa"
             target="_blank"
             rel="noreferrer"
           >
